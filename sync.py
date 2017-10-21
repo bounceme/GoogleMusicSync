e953cbfc7a833531ca0a41b4cab507da23fe1863
Chris Paul <chrispetrunoff@me.com>
2017-10-21 01:04:24 -0700

    Update sync.py

--- sync.py
+++ sync.py
@@ -4,30 +4,14 @@ import sys
 import time
 import re
 from mutagen.id3 import ID3, POPM
-print sys.argv
+from mutagen.mp4 import MP4
 
-def get_local_info(song_info):
-    song_info['rating'] = -1
-    song_info['title'] = song_info['filename']
-    song_info['artist'] = ''
-    if(song_info['type'] == 'mp3'):
-        try:
-            audio = ID3(song_info['path'])
-            rate_temp = audio.getall('POPM')
-            if(len(rate_temp) >= 1):
-                song_info['rating'] = rate_temp[0].rating
-            song_info['title'] = audio.get('TIT2').text[0]
-            song_info['artist'] = audio.get('TPE1').text[0]
-        except:
-            return
-    else:
-        return
+def mfor(path):
+    inf = MP4(path).tags
+    ti = inf['\xa9nam'][0]
+    ar = inf['\xa9ART'][0]
+    return [ti,ar]
 
-def get_remote_info(google_music_dict, song_info):
-    if (song_info['title'], song_info['artist']) in google_music_dict:
-        return google_music_dict[(song_info['title'], song_info['artist'])]
-    else:
-        return {}
 
 def build_dict():
     t = MusicDict()
@@ -56,15 +40,18 @@ class MusicDict():
             if(len(re.findall(".*(mp3|mp4|m4a)+", phile)) != 0):
                 file_info = {'path': os.path.join(path, phile), 'filename': phile}
                 if('m4a' in phile):
-                    file_info['type'] = 'm4a'
-                if('mp4' in phile):
-                    file_info['type'] = 'mp4'
-                if('mp3' in phile):
-                    file_info['type'] = 'mp3'
-                get_local_info(file_info)
-                self.songs[(file_info['title'], file_info['artist'])] = file_info
+                    va = mfor(file_info['path'])
+                    self.songs[(va[0],va[1])] = va
+                elif('mp4' in phile):
+                    va = mfor(file_info['path'])
+                    self.songs[(va[0],va[1])] = va
+                elif('mp3' in phile):
+                    audio = ID3(file_info['path'])
+                    file_info['title'] = audio.get('TIT2').text[0]
+                    file_info['artist'] = audio.get('TPE1').text[0]
+                    self.songs[(file_info['title'], file_info['artist'])] = file_info
                 self.size+=1
-                if(self.size % 1000 == 0):
+                if(self.size % 100 == 0):
                     print self.size, " songs found"
         for folder in folders:
             self.add_folder(os.path.join(path, folder))
@@ -87,23 +74,21 @@ print "Getting music from google"
 google_music = gmusicapi.Mobileclient()
 google_music.login(sys.argv[1],sys.argv[2],google_music.FROM_MAC_ADDRESS)
 google_songs = google_music.get_all_songs()
-google_songs_dict = {}
 print "Time taken", time.clock()-start
 
-print 'Putting google songs in a dictionary'
-for song in google_songs:
-    google_songs_dict[(song["title"].encode('ascii', 'ignore'), song["artist"].encode('ascii', 'ignore'))] = song
-print "Removing music from google play that does not exist on computer."
 delete_songs = []
+print 'Putting google songs in a dictionary'
+print "Removing music from google play that does not exist on computer."
 for song in google_songs:
-    if not music_dict.song_exists(song["title"].encode('ascii', 'ignore'), song["artist"].encode('ascii', 'ignore')):
-        print "Deleting "+song["title"].encode('ascii', 'ignore')+" by "+song["artist"].encode('ascii', 'ignore')+" remotely"
+    if not music_dict.song_exists(song["title"], song["artist"]):
+        print "Deleting "+song["title"]+" by "+song["artist"]+" remotely"
         #response = raw_input("Do you want to delete this song? (y/n): ")
         response = 'y'
         if(response!="n"):
                 delete_songs.append(song["id"])
 try:
-    google_music.delete_songs(delete_songs)
+    print delete_songs
+    # google_music.delete_songs(delete_songs)
 except Exception as e:
     print e
 
